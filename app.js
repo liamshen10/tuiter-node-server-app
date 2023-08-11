119f13273cd8a853a67b52b39eed22133b201132
liamshen10 <liamshen03@gmail.com>
2023-08-11 15:42:23 -0400

    fixwes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 const app = express()
 app.use(cors({
     credentials: true,
-    origin: 'https://a6--glowing-cocada-d3254c.netlify.app',
+    origin: process.env.FRONTEND_URL,
   })
  );
  const sessionOptions = {
