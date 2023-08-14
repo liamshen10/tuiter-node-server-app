a1280d93564827c80d2ef9be09281dec2009d798
liamshen10 <liamshen03@gmail.com>
2023-08-14 00:04:11 -0400

    debug

--- users/users-dao.js
+++ users/users-dao.js
@@ -10,7 +10,15 @@ export const findUserByCredentials = (username, password) =>
   usersModel.findOne({ username, password });
 export const createUser = (user) =>
   usersModel.create(user);
-export const updateUser = (id, user) =>
-  usersModel.updateOne({ _id: id }, { $set: user });
+export const updateUser = async (id, user) => {
+    try {
+      const result = await usersModel.updateOne({ _id: id }, { $set: user });
+      return result;
+    } catch (error) {
+      console.error("Error updating user:", error);
+      throw error;  // or return some error response
+    }
+  };
+  
 export const deleteUser = (id) =>
   usersModel.deleteOne({ _id: id });
