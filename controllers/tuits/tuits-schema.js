6f25667fccf932f821c3a5ab1c7bcdefdf776483
liamshen10 <liamshen03@gmail.com>
2023-08-14 00:11:45 -0400

    fixing tuit

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,19 @@
 import mongoose from 'mongoose';
-const schema = mongoose.Schema({
-  tuit: String,
-  likes: Number,
-  liked: Boolean,
-}, {collection: 'tuits'});
-export default schema;
+
+const tuitSchema = mongoose.Schema({
+    _id: String,             // MongoDB ID
+    topic: String,           // Topic of the tuit
+    username: String,        // Username of the tuit's author
+    handle: String,          // Handle of the tuit's author
+    time: String,            // Time since tuit was posted
+    image: String,           // Image associated with the tuit
+    title: String,           // Title of the tuit
+    tuit: String,            // Tuit content
+    liked: Boolean,          // Whether the tuit is liked or not
+    dislikes: Number,        // Number of dislikes
+    likes: Number,           // Number of likes
+    replies: Number,         // Number of replies
+    retuits: Number          // Number of retuits (shares)
+}, { collection: 'tuits' });
+
+export default tuitSchema;
